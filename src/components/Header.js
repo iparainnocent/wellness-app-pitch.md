fc5d4b4248781f63b4a98ac4aee37ce28c5f7357
Wambui Ndungu <wambuindungu4967@gmail.com>
2024-12-16 14:03:25 +0400

    styls the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,101 +35,93 @@ function Header({ activeSection, setActiveSection, setSearchQuery }) {
   };
 
   return (
-    <header ref={containerRef} className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        {/* Display the logo */}
-        <a className="navbar-brand" href="#">
-          <img
-            src="https://cdn3.vectorstock.com/i/1000x1000/23/32/wellness-center-logo-design-concept-spa-vector-33952332.jpg"
-            alt="Wellness Center Logo"
-            className="logo"
-          />
-          <h1 className="app-name">Haven Wellness Center</h1>
-        </a>
+<header ref={containerRef} className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    {/* Display the logo */}
+    <a className="navbar-brand" href="#">
+      <img
+        src="/images/wellness_logo.jpg"
+        alt="Wellness Center Logo"
+        className="logo"
+      />
+    </a>
+    
+    {/* App Name */}
+    <div className="app-name-container">
+      <h1 className="app-name">Haven Wellness Center</h1>
+    </div>
 
-        {/* Toggle button for small screens */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+    {/* Navigation and Actions Container */}
+    <div className="actions-container">
+      {/* Navigation links */}
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('about')}>
+            About
+          </button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('services')}>
+            Services
+          </button>
+        </li>
+        <li className="nav-item">
+          <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('contact')}>
+            Contact
+          </button>
+        </li>
+      </ul>
+
+      {/* Search form */}
+      <form className="d-flex me-3">
+        <input
+          className="form-control"
+          type="search"
+          placeholder="Search services..."
+          aria-label="Search services"
+          onChange={handleSearchChange} // Attach the handler
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
         </button>
+      </form>
 
-        {/* Navigation links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('about')}>
-                About
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('services')}>
-                Services
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link btn btn-link" onClick={() => handleSectionToggle('contact')}>
-                Contact
-              </button>
-            </li>
-          </ul>
+      {/* Login Button */}
+      <button className="btn btn-outline-primary ms-2" onClick={toggleLoginForm}>
+        Login
+      </button>
+    </div>
+  </div>
 
-          {/* Search form */}
-          <form className="d-flex me-3">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Search services..."
-              aria-label="Search services"
-              onChange={handleSearchChange} // Attach the handler
-            />
-         
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-
-          {/* Login Button */}
-          <button className="btn btn-outline-primary ms-2" onClick={toggleLoginForm}>
+  {/* Conditional rendering of the login form */}
+  {showLogin && (
+    <div className="login-form-overlay">
+      <div className="login-form">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input type="email" className="form-control" id="email" placeholder="Enter email" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="password" placeholder="Enter password" />
+          </div>
+          <button type="submit" className="btn btn-primary">
             Login
           </button>
-        </div>
+          <button type="button" className="btn btn-secondary ms-2" onClick={toggleLoginForm}>
+            Close
+          </button>
+        </form>
       </div>
+    </div>
+  )}
+</header>
 
-      {/* Conditional rendering of the login form */}
-      {showLogin && (
-        <div className="login-form-overlay">
-          <div className="login-form">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                  Email address
-                </label>
-                <input type="email" className="form-control" id="email" placeholder="Enter email" />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="password" className="form-label">
-                  Password
-                </label>
-                <input type="password" className="form-control" id="password" placeholder="Enter password" />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Login
-              </button>
-              <button type="button" className="btn btn-secondary ms-2" onClick={toggleLoginForm}>
-                Close
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
-    </header>
   );
 }
 
